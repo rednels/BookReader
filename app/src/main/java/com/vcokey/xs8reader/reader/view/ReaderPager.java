963791e4af54479370eca29b5417ab49d055b590
liwubin <liwubin@hongxiu.com>
2015-09-11 18:22:11 +0800

    开始实现翻页

---
+++ app/src/main/java/com/vcokey/xs8reader/reader/view/ReaderPager.java
@@ -0,0 +1,25 @@
+package com.vcokey.xs8reader.reader.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by hongxiu on 2015/9/11.
+ */
+public class ReaderPager extends ViewGroup {
+
+    public ReaderPager(Context context) {
+        this(context,null);
+    }
+
+    public ReaderPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
